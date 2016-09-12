89e773a4fe7fd2c70566a49936c66311d3025f5c
Alexander Novikov <alexander@pubnub.com>
2016-09-12 13:02:45 -0700

    Init commit

---
+++ script.js
@@ -0,0 +1,157 @@
+$(function () {
+    var EVENTS_CHANNEL = "pubnub_demo_api_python_events",
+        PUBLISH_SYNC_PATH = '/subscription/publish/sync',
+        PUBLISH_ASYNC_PATH = '/subscription/publish/async',
+        PUBLISH_ASYNC2_PATH = '/subscription/publish/async2',
+        SUBSCRIPTION_ADD_PATH = '/subscription/add',
+        SUBSCRIPTION_REMOVE_PATH = '/subscription/remove',
+        SUBSCRIPTION_LIST_PATH = '/subscription/list',
+        apiUrl;
+
+    var pubnub = new PubNub({
+        subscribeKey: "sub-c-33f55052-190b-11e6-bfbc-02ee2ddab7fe",
+        publishKey: "pub-c-739aa0fc-3ed5-472b-af26-aca1b333ec52"
+    });
+
+    pubnub.addListener({
+        status: function (statusEvent) {
+            status(JSON.stringify(statusEvent))
+        },
+        presence: function (presenceEvent) {
+            presence(JSON.stringify(presenceEvent))
+        },
+        message: function (message) {
+            console.log('hey');
+            status(JSON.stringify(message))
+        }
+    });
+
+    pubnub.subscribe({
+        channels: [EVENTS_CHANNEL ]
+    });
+
+    $("#add-channel-button").on('click', function (e) {
+        e.preventDefault();
+
+        var val = $("#add-channel-input").val();
+
+        $.get(apiUrl + SUBSCRIPTION_ADD_PATH, {channel: val})
+            .done(function(msg) {
+                output(JSON.stringify(msg))
+            })
+            .fail(function(e) {
+                output("Error: " + JSON.stringify(e))
+            })
+    });
+
+    $("#remove-channel-button").on('click', function (e) {
+        e.preventDefault();
+
+        var val = $("#remove-channel-input").val();
+
+        $.get(apiUrl + SUBSCRIPTION_REMOVE_PATH, {channel: val})
+            .done(function(msg) {
+                output(JSON.stringify(msg))
+            })
+            .fail(function(e) {
+                output("Error: " + JSON.stringify(e))
+            })
+    });
+
+    $("#list-channels-button").on('click', function (e) {
+        e.preventDefault();
+
+        $.get(apiUrl + SUBSCRIPTION_LIST_PATH)
+            .done(function(msg) {
+                output(JSON.stringify(msg))
+            })
+            .fail(function(e) {
+                output("Error: " + JSON.stringify(e))
+            })
+    });
+
+    $("#publish-sync-button").on('click', function (e) {
+        e.preventDefault();
+
+        $.get(apiUrl + PUBLISH_SYNC_PATH)
+            .done(function(msg) {
+                output(JSON.stringify(msg))
+            })
+            .fail(function(e) {
+                output("Error: " + JSON.stringify(e))
+            })
+    });
+
+    $("#publish-async-button").on('click', function (e) {
+        e.preventDefault();
+
+        $.get(apiUrl + PUBLISH_ASYNC_PATH)
+            .done(function(msg) {
+                output(JSON.stringify(msg))
+            })
+            .fail(function(e) {
+                output("Error: " + JSON.stringify(e))
+            })
+    });
+
+    $("#publish_async2-button").on('click', function (e) {
+        e.preventDefault();
+
+        $.get(apiUrl + PUBLISH_ASYNC2_PATH)
+            .done(function(msg) {
+                output(JSON.stringify(msg))
+            })
+            .fail(function(e) {
+                output("Error: " + JSON.stringify(e))
+            })
+    });
+
+    $("#api-url").change(function () {
+        var val = $("#api-url option:selected").val();
+
+        if (val !== 'custom') {
+            apiUrl = val;
+        } else {
+            apiUrl = $("custom-api-url").val()
+        }
+    }).change();
+
+    $("#clear-output").on('click', function (e) {
+        $("#output").val('');
+    });
+
+    $("#clear-status").on('click', function (e) {
+        $("#status").val('');
+    });
+
+    $("#clear-presence").on('click', function (e) {
+        $("#presence").val('');
+    });
+
+    function output(val) {
+        var field = $("#output"),
+            current = field.val();
+
+        var updated = val + '\n' + current;
+
+        field.val(updated);
+    }
+
+    function status(val) {
+        var field = $("#status"),
+            current = field.val();
+
+        var updated = val + '\n' + current;
+
+        field.val(updated);
+    }
+
+    function presence(val) {
+        var field = $("#presence"),
+            current = field.val();
+
+        var updated = val + '\n' + current;
+
+        field.val(updated);
+    }
+});
